25e15c452751a0613179a2a553440b4e09f4a68f
Frane Suman <100610061+fsuman20@users.noreply.github.com>
2024-01-24 03:03:40 +0100

    initial upload

---
+++ RecenzijeAplikacija.py
@@ -0,0 +1,90 @@
+import pandas as pd
+import pickle
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import seaborn as sns
+import matplotlib.pyplot as plt
+import os.path
+
+def napravi_recenziju():
+    """
+    Funkcija za stvaranje nove recenzije.
+
+    Ova funkcija učitava prethodno obučeni model i vektorizator, traži od korisnika da unese novi pregled,
+    pretprocesira tekst, vektorizira recenziju, predviđa ocjenu i sprema recenziju u CSV datoteku.
+
+    Parameters:
+    None
+
+    Returns:
+    None
+    """
+    # Učitavanje modela i vektorizatora
+    with open('model_Restaurant.pkl', 'rb') as datoteka:
+        model = pickle.load(datoteka)
+    with open('vektorizator_Restaurant.pkl', 'rb') as datoteka:
+        vektorizator = pickle.load(datoteka)
+
+    # Unos nove recenzije
+    nova_recenzija = input("Unesite novu recenziju: ")
+
+    # Predobrada teksta
+    zaustavne_rijeci = set(stopwords.words('english'))
+    nova_recenzija = ' '.join([rijec for rijec in word_tokenize(nova_recenzija) if rijec not in zaustavne_rijeci])
+
+    # Vektorizacija recenzije
+    X_nova = vektorizator.transform([nova_recenzija])
+
+    # Predviđanje ocjene
+    predikcija = model.predict(X_nova)
+
+    #ako je ocjena 1 ili 2, recenzija je "kritika" inače je "pohvala"
+    if predikcija[0] == 1 or predikcija[0] == 2:
+        informacija = "(Kritika)"
+    if predikcija[0] == 3:
+        informacija = "(Neutralna)"
+    else:
+        informacija = "(Pohvala)"
+
+    # Ispis ocjene
+    print("Ocjena recenzije je:", predikcija[0], informacija)
+
+    # Spremanje recenzije i ocjene u CSV datoteku
+    df_nova = pd.DataFrame({'Review': [nova_recenzija], 'Rating': [predikcija[0]]})
+    if not os.path.isfile('nove_recenzije.csv'):
+        df_nova.to_csv('nove_recenzije.csv', index=False)
+    else: # inače se dodaje na postojeću datoteku
+        df_nova.to_csv('nove_recenzije.csv', mode='a', header=False, index=False)
+
+def prikaz_grafa():
+    """
+    Funkcija za prikaz grafa.
+
+    Ova funkcija učitava prethodno spremljene recenzije i prikazuje broj recenzija po ocjenama.
+
+    Parameters:
+    None
+
+    Returns:
+    None
+    """
+    try:
+        df_nova = pd.read_csv('nove_recenzije.csv')
+        sns.countplot(x='Rating', data=df_nova)
+        plt.xlabel('Ocjena recenzije')
+        plt.ylabel('Broj recenzija')
+        plt.title('Broj recenzija po ocjenama')
+        plt.show()
+    except:
+         print("Nema recenzija!")
+
+
+# Switch case
+option = input("Odaberite: 1) Napravi novu recenziju, 2) Prikaži podatke : ")
+
+if option == "1":
+    napravi_recenziju()
+elif option == "2":
+    prikaz_grafa()
+else:
+    print("Nepostojeća opcija!")
---
+++ model_review.py
@@ -0,0 +1,82 @@
+# Učitavanje potrebnih biblioteka
+import pandas as pd
+import pickle
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import accuracy_score, confusion_matrix
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from imblearn.over_sampling import RandomOverSampler
+from sklearn.naive_bayes import MultinomialNB
+
+# Učitavanje podataka
+df = pd.read_csv('Restaurant reviews.csv')
+
+# Transformacija i čišćenje podataka
+df['Rating'] = df['Rating'].replace({'Like':3})
+df['Rating'] = df['Rating'].fillna(df['Rating'].median())
+df['Rating'] = df['Rating'].astype(float).round().astype(int)
+
+# Eksplorativna analiza podataka
+print(df.info())
+print(df.describe())
+
+# Vizualizacija podataka
+sns.countplot(x='Rating', data=df)
+plt.xlabel('Ocjena recenzije')
+plt.ylabel('Broj recenzija')
+plt.title('Broj recenzija po ocjenama')
+plt.show()
+
+# Tokenizacija i vektorizacija
+zaustavne_rijeci = set(stopwords.words('english'))
+df['Review'] = df['Review'].apply(lambda x: ' '.join([rijec for rijec in word_tokenize(x) if rijec not in zaustavne_rijeci]))
+
+vektorizator = TfidfVectorizer()
+X = vektorizator.fit_transform(df['Review'])
+
+# Balansiranje klasa
+ros = RandomOverSampler(random_state=0)
+X_resampled, y_resampled = ros.fit_resample(X, df['Rating'])
+
+# Podjela podataka na trening i test set
+X_trening, X_test, y_trening, y_test = train_test_split(X_resampled, y_resampled, test_size=0.2, random_state=42)
+
+
+# Definiranje parametara za pretragu
+parametri = {'alpha': [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]}
+
+# Inicijalizacija modela
+model = MultinomialNB()
+
+# Inicijalizacija GridSearchCV
+grid_search = GridSearchCV(model, parametri, cv=5, scoring='accuracy')
+
+# Treniranje modela koristeći GridSearchCV
+grid_search.fit(X_trening, y_trening)
+
+# Ispis najboljih parametara
+print('Najbolji parametri:', grid_search.best_params_)
+
+model = MultinomialNB(alpha=0.1)
+model.fit(X_trening, y_trening)
+
+# Evaluacija modela
+y_predikcija = model.predict(X_test)
+print('Točnost modela:', accuracy_score(y_test, y_predikcija))
+print('Matrica konfuzije:\n', confusion_matrix(y_test, y_predikcija))
+
+# Unakrsna validacija
+ocjene = cross_val_score(model, X_resampled, y_resampled, cv=5)
+print('Unakrsne ocjene:', ocjene)
+print('Prosječna ocjena:', ocjene.mean())
+
+# Spremanje modela
+with open('model_Restaurant.pkl', 'wb') as datoteka:
+    pickle.dump(model, datoteka)
+
+# Spremanje vektorizatora
+with open('vektorizator_Restaurant.pkl', 'wb') as datoteka:
+    pickle.dump(vektorizator, datoteka)
